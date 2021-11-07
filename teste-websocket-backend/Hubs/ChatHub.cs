6edb43da8218a2635c4b40d051e1d4a396202ad4
Ricardo Milan <ricardo@intercambiodireto.com>
2021-11-07 13:31:43 -0300

    Add project files.

---
+++ teste-websocket-backend/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using teste_websocket_backend.Models;
+
+namespace teste_websocket_backend.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+        public ChatHub()
+        {
+            _botUser = "Bot User";
+        }
+
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            string method = "ReceiveMessage"; // nome da função/método do frontend            
+            
+            //await Clients.All // envia para todos os clients
+            //    .SendAsync(method, _botUser, $"{userConnection.User} has joined {userConnection.Room}");
+            
+            await Clients.Group(userConnection.Room) // envia para clients no grupo
+                .SendAsync(method, _botUser, $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
